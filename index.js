a0cd75cf4b38e90e66e55881e1c9b832b57e857a
Ariana <arianacabral57@gmail.com>
2023-01-03 15:00:29 -0300

    🍄 Add audio

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const panels = document.querySelectorAll(".panel");
 
+let audio_play = document.getElementsByTagName("audio");
+console.log(audio_play);
+
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
     removeActiveClasses();
     panel.classList.add("active");
+    audio_play[0].play();
   });
 });
 
